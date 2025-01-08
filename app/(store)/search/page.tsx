039b31e66c03bb14791a20759142b15a586ce7f1
kabornblack <shaibu.kabiru@email.com>
2025-01-09 01:44:19 +0200

    Header done with auth, passkeys and searchParams

---
+++ app/(store)/search/page.tsx
@@ -0,0 +1,13 @@
+async function SearchPage({
+  searchParams,
+}: {
+  searchParams: {
+    query: string;
+  };
+}) {
+  const { query } = await searchParams;
+
+  return <div>Search page for {query}</div>;
+}
+
+export default SearchPage;
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,25 +5,35 @@ import Link from "next/link";
 import React from "react";
 import Form from "next/form";
 import { PackageIcon, TrolleyIcon } from "@sanity/icons";
+import { SignedIn } from "@clerk/clerk-react";
 
 function Header() {
   const { user } = useUser();
+
+  const createClerkPasskeys = async () => {
+    try {
+      const response = await user?.createPasskey();
+      console.log(response);
+    } catch (err) {
+      console.error("Error:", JSON.stringify(err, null, 2));
+    }
+  };
 
   console.log(user);
   return (
     <header className="flex flex-wrap justify-between items-center px-4 py-2">
       {/* Top row */}
-      <div>
+      <div className="flex w-full flex-wrap justify-between items-center px-4 py-2">
         <Link
           href="/"
           className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
         >
-          Shoper
+          Shopr
         </Link>
 
         <Form
           action="/search"
-          className="w-full sm:w-auto sm:flex-1 sm:mx-4 sm:mt-0"
+          className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
         >
           <input
             type="text"
@@ -33,7 +43,7 @@ function Header() {
             focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 border w-full max-w-4xl"
           />
         </Form>
-        <div>
+        <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
           <Link
             href="/basket"
             className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center
@@ -48,7 +58,7 @@ function Header() {
 
           {/* User area */}
           <ClerkLoaded>
-            {user && (
+            <SignedIn>
               <Link
                 href="/orders"
                 className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center
@@ -56,7 +66,7 @@ function Header() {
               >
                 <PackageIcon className="w-6 h-6" /> <span>My Order</span>
               </Link>
-            )}
+            </SignedIn>
 
             {user ? (
               <div className="flex items-center space-x-2">
@@ -69,6 +79,16 @@ function Header() {
               </div>
             ) : (
               <SignInButton mode="modal" />
+            )}
+
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskeys}
+                className="bg-white hover:bg-blue-700 hover:text-white animate-pulse
+                 text-blue-500 font-bold py-2 px-4 rounded border-blue-300 border"
+              >
+                Create passkey
+              </button>
             )}
           </ClerkLoaded>
         </div>
